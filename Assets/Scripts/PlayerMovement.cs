1a9f5c2d2cc663525c7a97b7c3f164097eeed211
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-09-19 15:46:48 +0630

    first commit museum sign and dissolve shader

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float hello;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
